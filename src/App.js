980ff150275c8d83c205e55f835c1c2c1dbd4c5a
Csonic90 <sylwester.fron@gmail.com>
2021-02-10 22:35:40 +0100

    add logic calc and css calc

--- src/App.js
+++ src/App.js
@@ -1,23 +1,71 @@
-import logo from './logo.svg';
+import React, { useState } from 'react'
 import './App.css';
 
 function App() {
+
+  const [data, setData] = useState("");
+  const [eql, setEql] = useState("");
+
+  const changeSign = () => {
+    setEql(String(eql === "" ? (eval(data) * (-1)) : (eval(eql) * -1)))
+  }
+
+  const useEql = (e) => {
+    try {
+      setEql(
+        String(eval(data)).length > 3 &&
+          String(eval(data)).includes(".")
+          ? String(eval(data).toFixed(4))
+          : String(eval(data))
+      );
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+  const buttonEfect = (e) => {
+    setData(data + e.target.value)
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="calc-wrapper">
+        <div className="input">
+          <div className="input-data">{data}</div>
+          <div className="eql">{eql}</div>
+        </div>
+        <div className="row">
+          <button className="button-wrapper" onClick={buttonEfect} value="%">%</button>
+          <button className="button-wrapper" onClick={changeSign} >+/-</button>
+          <button className="button-wrapper" onClick={() => (setData(""), setEql(""))}>C</button>
+          <button className="button-wrapper operator" onClick={buttonEfect} value=" / ">/</button>
+        </div>
+        <div className="row">
+          <button className="button-wrapper" onClick={buttonEfect} value="7" >7</button>
+          <button className="button-wrapper" onClick={buttonEfect} value="8" >8</button>
+          <button className="button-wrapper" onClick={buttonEfect} value="9" >9</button>
+          <button className="button-wrapper operator" onClick={(e) => setData(data + e.target.value)} value=" * " >x</button>
+        </div>
+        <div className="row">
+          <button className="button-wrapper" onClick={(e) => setData(data + e.target.value)} value="4" >4</button>
+          <button className="button-wrapper" onClick={(e) => setData(data + e.target.value)} value="5" >5</button>
+          <button className="button-wrapper" onClick={(e) => setData(data + e.target.value)} value="6" >6</button>
+          <button className="button-wrapper operator" onClick={(e) => setData(data + e.target.value)} value=" - " >-</button>
+        </div>
+        <div className="row">
+          <button className="button-wrapper" onClick={(e) => setData(data + e.target.value)} value="1">1</button>
+          <button className="button-wrapper" onClick={(e) => setData(data + e.target.value)} value="2">2</button>
+          <button className="button-wrapper" onClick={(e) => setData(data + e.target.value)} value="3">3</button>
+          <button className="button-wrapper operator" onClick={(e) => setData(data + e.target.value)} value=" + ">+</button>
+        </div>
+        <div className="row">
+          <button className="button-wrapper" onClick={(e) => setData(data + e.target.value)} value="0">0</button>
+          <button className="button-wrapper" onClick={(e) => setData(data + e.target.value)} value=".">,</button>
+          <button className="button-wrapper equale"
+            onClick={useEql}>=</button>
+        </div>
+      </div>
     </div>
   );
 }
